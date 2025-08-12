068f40aff2726b662cecb6ac18ea2b41b88d2562
olivier Rukundo <rr.olivier01@gmail.com>
2025-08-12 17:36:29 +0200

    Added caching comments for description

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 // Redis is an in-memory key-value store
 //it's extremely fast, so instead of always fetching from Mysql/MangoDB
 //you can store data temprarily in Redis and retrieve it instantly
+
+
+//Reason for caching
+// 1.speed: Fetching from Redis can be 10*-100** 
+// 2.Reduced Load:less stress on your main database  
+// 3.cost saving: fewer expensive DB queries
